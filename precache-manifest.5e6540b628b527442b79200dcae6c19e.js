f887b48a088da0bfbfe84b05be32fad32c4fbeec
moonfalljade <moonfalljade@hotmail.com>
2019-04-24 07:08:41 +0800

    Updates

---
+++ precache-manifest.5e6540b628b527442b79200dcae6c19e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "1b7e853e01d73a6cc24d",
+    "url": "/myreads/static/js/runtime~main.1b7e853e.js"
+  },
+  {
+    "revision": "66cc544a890fa264968d",
+    "url": "/myreads/static/js/main.66cc544a.chunk.js"
+  },
+  {
+    "revision": "42ae268105e34bac3a35",
+    "url": "/myreads/static/js/1.42ae2681.chunk.js"
+  },
+  {
+    "revision": "66cc544a890fa264968d",
+    "url": "/myreads/static/css/main.b4782130.chunk.css"
+  },
+  {
+    "revision": "b2a77bfd139903c384e398625a657a3d",
+    "url": "/myreads/index.html"
+  }
+];
